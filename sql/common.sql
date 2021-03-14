4a106ae398d9bfcdd2cf5f435c6eac7d57c291b0
Charlie <51788929+Charlie-niu@users.noreply.github.com>
2021-03-14 21:11:32 +0800

    Update common.sql

--- sql/common.sql
+++ sql/common.sql
@@ -1,1 +1,12 @@
+DROP TABLE IF EXISTS `com_area`;
 
+CREATE TABLE `com_area`
+(`areaID` INT(125) NOT NULL AUTO_INCREMENT,
+`areaName` VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL,
+`areaLevel` INT(10),
+`areaCode` INT(125),
+`parentCode` INT(125),
+`createDate` DATETIME,
+`changeDate` DATETIME,
+PRIMARY KEY (`areaID`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
